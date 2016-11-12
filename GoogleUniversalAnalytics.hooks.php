989d8208b27b9b48d29e8f7cad5733b980b002af
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2016-11-13 00:06:31 +0200

    Explicitly declare getBasicSnippet() as static (aargh!)

--- GoogleUniversalAnalytics.hooks.php
+++ GoogleUniversalAnalytics.hooks.php
@@ -131,7 +131,7 @@ class GoogleUniversalAnalyticsHooks {
 		return $script;
 	}
 
-	private function getBasicSnippet() {
+	private static function getBasicSnippet() {
 		$snippet = <<<SNIPPET
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
