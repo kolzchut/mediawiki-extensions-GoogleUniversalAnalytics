6ec78d8cef15a6d5be27af264d4823b6a9347d09
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2018-03-05 03:34:12 +0200

    Convert to Extension Registration (extension.json)

--- GoogleUniversalAnalytics.hooks.php
+++ GoogleUniversalAnalytics.hooks.php
@@ -188,23 +188,4 @@ SNIPPET;
 		return PHP_EOL . '<!-- ' . wfMessage( $messageName )->text() . ' -->' . PHP_EOL;
 
 	}
-
-	public static function onUnitTestsList( array &$files ) {
-		// @codeCoverageIgnoreStart
-		$directoryIterator = new RecursiveDirectoryIterator( __DIR__ . '/tests/' );
-
-		/**
-		 * @var SplFileInfo $fileInfo
-		 */
-		$ourFiles = [];
-		foreach ( new RecursiveIteratorIterator( $directoryIterator ) as $fileInfo ) {
-			if ( substr( $fileInfo->getFilename(), -8 ) === 'Test.php' ) {
-				$ourFiles[] = $fileInfo->getPathname();
-			}
-		}
-
-		$files = array_merge( $files, $ourFiles );
-		return true;
-		// @codeCoverageIgnoreEnd
-	}
 }
