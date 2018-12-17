90f367c174c53881a089a7a2a2bc77a521b3f967
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2018-12-17 19:33:17 +0200

    Identify phone links as well
    This uses the class 'phonenum' added by extension:PhoneLinks,
    and not detection by href="tel:".

--- modules/ext.googleUniversalAnalytics.externalLinks.js
+++ modules/ext.googleUniversalAnalytics.externalLinks.js
@@ -1,17 +1,25 @@
 ( function ( $, mw ) {
 	var gaUtils = mw.googleAnalytics.utils;
 
+	function identifyLinkType( $element ) {
+		if ( $element.hasClass( 'external' ) ) {
+			if ( $element.hasClass( 'phonenum' ) ) {
+				return 'Phone Links';
+			}
+			return 'Outbound Links';
+		}
+		if ( $element.hasClass( 'extiw' ) ) {
+			return 'Outbound Interwiki';
+		}
+
+		// None of the above, so it must be an interlanguage link we picked up
+		return 'Language Links';
+	}
+
 	$( 'body' ).on( 'click', 'a.external, a.extiw, .interlanguage-link > a', function ( e ) {
 		var targetUrl = $( this ).attr( 'href' ),
 			targetHost = e.currentTarget.host.replace( /:\d{2,4}/g, '' ),
-			linkType;
-		if ( $( this ).hasClass( 'external' ) ) {
-			linkType = 'Outbound Links';
-		} else if ( $( this ).hasClass( 'extiw' ) ) {
-			linkType = 'Outbound Interwiki';
-		} else {
-			linkType = 'Language Links';
-		}
+			linkType = identifyLinkType( $( this ) );
 
 		gaUtils.recordClickEvent( e, {
 			eventCategory: linkType,
